5b1296ce34c72f99eecc8e5a1b3bec60308c3db5
Benjamin Lupton <b@lupton.cc>
2013-10-23 17:13:07 +1100

    Updated to use caterpillar example logs

--- example.js
+++ example.js
@@ -16,8 +16,8 @@ var output = logger
 
 // Export
 module.exports = {
-	logger:logger,
-	log:logger.log.bind(logger)
+	logger: logger,
+	log: logger.log.bind(logger)
 };
 
 
@@ -27,8 +27,20 @@ module.exports = {
 // Import
 // var logger = require('./logger')
 
-// Log
-var logLevel, _i;
-for (logLevel = _i = 0; _i <= 7; logLevel = ++_i) {
-	logger.log(logLevel, "this is log level " + logLevel);
-}
+// Log messages
+logger.log('emergency', 'this is level 0');
+logger.log('emerg', 'this is level 0');
+logger.log('alert', 'this is level 1');
+logger.log('critical', 'this is level 2');
+logger.log('crit', 'this is level 2');
+logger.log('error', 'this is level 3');
+logger.log('err', 'this is level 3');
+logger.log('warning', 'this is level 4');
+logger.log('warn', 'this is level 4');
+logger.log('notice', 'this is level 5');
+logger.log('note', 'this is level 5');
+logger.log('info', 'this is level 6');
+logger.log('default', 'this is level 6');
+logger.log('debug', 'this is level 7');
+logger.log('this is level 6, the default level');
+logger.log('you','can','also','use','as','many','arguments','as','you','want',1,[2,3],{four:5});
